554612096c71daba5fb5e23148abd6f6e2957026
Santy <pizarro.santy@gmail.com>
2024-10-22 18:06:45 -0300

    lobby primer commit

---
+++ app/controller/LobbyController.php
@@ -0,0 +1,18 @@
+<?php
+
+class LobbyController{
+
+    private $model;
+    private $presenter;
+
+    public function __construct($model, $presenter)
+    {
+        $this->model = $model;
+        $this->presenter = $presenter;
+    }
+
+    public function mostrarLobby(){
+
+    }
+
+}
