0425ea6a9c9c20b13fdde9b0570107871e3b6dff
IgnacioMoyano <110944396+IgnacioMoyano@users.noreply.github.com>
2024-10-24 14:56:34 -0300

    vista_&_funcion_Ruleta

---
+++ app/public/js/ruleta.js
@@ -0,0 +1,67 @@
+function wheelOfFortune(selector) {
+    const node = document.querySelector(selector);
+    if (!node) return;
+
+    const spin = node.querySelector('button');
+    const wheel = node.querySelector('ul');
+    const items = ['3', '2', '1', '8', '7', '6', '5', '4']; // Las 8 casillas
+    let animation;
+    let previousEndDegree = 0;
+
+    spin.addEventListener('click', () => {
+
+        spin.disabled = true;
+
+
+        const randomAdditionalDegrees = Math.random() * 360 + 1800;
+        const newEndDegree = previousEndDegree + randomAdditionalDegrees;
+
+        animation = wheel.animate([
+            { transform: `rotate(${previousEndDegree}deg)` },
+            { transform: `rotate(${newEndDegree}deg)` }
+        ], {
+            duration: 4000,
+            direction: 'normal',
+            easing: 'cubic-bezier(0.440, -0.205, 0.000, 1.130)',
+            fill: 'forwards',
+            iterations: 1
+        });
+
+        previousEndDegree = newEndDegree;
+
+
+        animation.onfinish = () => {
+            const adjustedDegrees = (newEndDegree % 360) + (360 / items.length) / 2;
+            const selectedIndex = Math.floor((adjustedDegrees / 360) * items.length) % items.length;
+            const selectedValue = items[selectedIndex];
+
+            console.log(newEndDegree);
+            console.log('Casilla seleccionada:', selectedValue);
+
+
+            fetch('/Monquiz/app/juego/resultado', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    valor: selectedValue
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Respuesta del servidor:', data);
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                })
+                .finally(() => {
+
+                    spin.disabled = false;
+                });
+        };
+    });
+}
+
+// Uso
+wheelOfFortune('.ui-wheel-of-fortune');
